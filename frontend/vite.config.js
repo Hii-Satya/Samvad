fb118b2de7fc7e6b40acdf3f0580d1eeff6b25e4
Hii-Satya <iamsatyaprakash27@gmail.com>
2025-07-01 17:59:06 +0530

    project completed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    chunkSizeWarningLimit: 1000, // set to 1000 KB (1 MB), or higher as needed
+  },
 })
