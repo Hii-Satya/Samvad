c832ccf53983d11e90cb62459cb335c42b955b52
Hii-Satya <iamsatyaprakash27@gmail.com>
2025-07-01 18:21:13 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,10 +14,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-// const __dirname = path.resolve();
-
-import { fileURLToPath } from "url";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,10 +29,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
